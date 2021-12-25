394f24fb3dd24d7b170dc52658a55d3c9e7934a4
RavinduSathsara <rsathsara330@gmail.com>
2021-12-25 08:21:17 +0530

    Setup Sass to the project

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
-import "./index.css";
+import "./sass/index.scss";
 import { BrowserRouter } from "react-router-dom";
 ReactDOM.render(
   <BrowserRouter>
